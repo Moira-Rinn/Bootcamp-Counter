dae2141a07b044c67bcc265e889e59c9677c2073
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-08-22 00:22:12 -0400

    Added base functionality.

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "b'\xb6\xffE\\\x9eg5*nL\xaf\xf8j*q"
 
 
 @app.route('/')
 def index():
+    session['visits'] = session['visits'] + 1
     return render_template("index.html")
 
 
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     print("Got Post Info")
-#     print(request.form)
-#     return redirect('/')
+@app.route('/count', methods=['POST'])
+def count():
+    return redirect('/')
+
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    session['visits'] = 0
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
